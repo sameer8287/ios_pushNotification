230b761395da3e91c36c8f9375e69a47f00cb9d7
sameer_yusuf <sameer.y@extensioncrm.com>
2023-08-03 10:59:38 +0530

    first commit

---
+++ lib/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: const Center(
+        child: Text('Ios_push_notification'),
+      ),
+    );
+  }
+}
